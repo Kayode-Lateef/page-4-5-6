b080c6515c135392ae026f077f7847b19dceb853
Kayode-Lateef <69173278+Kayode-Lateef@users.noreply.github.com>
2021-05-01 02:03:50 +0100

    Add files via upload

--- demo.js
+++ demo.js
@@ -9,12 +9,36 @@
   fullHeight();
   $("#sidebarCollapse").on("click", function () {
     $("#sidebar").toggleClass("active");
-    // $('.collapse.in').toggleClass('in');
-    // $(".logo").css("display", "block");
+  
     if( $(".logo").css('visibility') == 'hidden')
     $(".logo").css({opacity: 1.0, visibility: "visible"}).animate({opacity: 0}, 5000);
     else
     $(".logo").css({opacity: 0.0, visibility: "hidden"}).animate({opacity: 1}, 2000);
   });
-})(jQuery);
+
+
+  $(".txt-1").on("click", function () {
+  $(".txt-1").css({color: "#061541", fontSize: "28px", fontWeight: 700, opacity: "1.0" });
+  $(".txt-2").css({color: "#061541", fontSize: "18px", fontWeight: 400, opacity: "0.5" });
+  $(".txt-3").css({color: "#061541", fontSize: "18px", fontWeight: 400, opacity: "0.5" });
+
+});
+
+$(".txt-2").on("click", function () {
+$(".txt-2").css({color: "#061541", fontSize: "28px",  fontWeight: 700, opacity: "1.0" });
+$(".txt-1").css({color: "#061541", fontSize: "18px", fontWeight: 400, opacity: "0.5" });
+$(".txt-3").css({color: "#061541", fontSize: "18px", fontWeight: 400, opacity: "0.5" });
+
+});
+
+$(".txt-3").on("click", function () {
+  $(".txt-3").css({color: "#061541", fontSize: "28px",  fontWeight: 700, opacity: "1.0" });
+  $(".txt-1").css({color: "#061541", fontSize: "18px",  fontWeight: 400, opacity: "0.5" });
+  $(".txt-2").css({color: "#061541", fontSize: "18px",  fontWeight: 400, opacity: "0.5" });
+});
+
+
+
+})
+(jQuery);
 
